3c8403d5a8472345bb432902b18262ab07c8038d
Dan Tamas <hi@dan-tamas.me>
2018-04-25 19:58:54 +0200

    add inputs to signUp screen

--- src/screens/signUp.js
+++ src/screens/signUp.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
 
 import { COLORS } from '../../src/lib/theme';
+
+const { width } = Dimensions.get('window');
 
 class SignUpScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>sign up</Text>
+                <TextInput style={styles.input} placeholder={'Your Name'} placeholderTextColor={COLORS.lightText} />
+                <TextInput style={styles.input} placeholder={'Your Email'} placeholderTextColor={COLORS.lightText} />
+                <TextInput style={styles.input} placeholder={'Password'} placeholderTextColor={COLORS.lightText} />
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Confirm Password'}
+                    placeholderTextColor={COLORS.lightText}
+                />
             </View>
         );
     }
@@ -25,6 +35,16 @@ const styles = StyleSheet.create({
         fontSize: 40,
         textAlign: 'center',
         margin: 10
+    },
+    input: {
+        margin: 10,
+        height: 30,
+        color: COLORS.lightText,
+        padding: 5,
+        borderColor: COLORS.borderColor,
+        borderRadius: 10,
+        borderWidth: 1,
+        width: width * 0.6
     }
 });
 
