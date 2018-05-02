a5b3a586beee47aaf7f5e814b729059caebe07d4
Dan Tamas <hi@dan-tamas.me>
2018-05-02 21:14:42 +0200

    add chatRooms list

--- src/lib/chat.js
+++ src/lib/chat.js
@@ -1,18 +1,15 @@
 const rooms = [
-	{
-		name: 'Kickass Team 💪',
-		lastMessageTime:
-			'10:45'
-	},
-	{
-		name: 'Best Friends 🤪',
-		lastMessageTime:
-			'21:07'
-	},
-	{
-		name: 'Haloween Party 🎃',
-		lastMessageTime:
-			'00:00'
-	}
+    {
+        name: 'Kickass Team 💪',
+        lastMessageTime: '10:45'
+    },
+    {
+        name: 'Best Friends 🤪',
+        lastMessageTime: '21:07'
+    },
+    {
+        name: 'Haloween Party 🎃',
+        lastMessageTime: '01:10'
+    }
 ];
 export { rooms };
--- src/screens/chatRooms.js
+++ src/screens/chatRooms.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, Platform } from 'react-native';
 
 import { COLORS, SIZES } from '../../src/lib/theme';
+
+import { rooms } from '../lib/chat';
 
 class ChatRoomsScreen extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.title}>ChatRooms</Text>
-            </View>
+            <FlatList
+                style={styles.flatList}
+                data={rooms}
+                keyExtractor={(item, idx) => {
+                    return `roomItem_${idx}`;
+                }}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.listItem}>
+                            <Text style={styles.name}>{`${item.name}`}</Text>
+                            <Text style={styles.time} numberOfLines={1} ellipsizeMode={'tail'}>
+                                {`${item.lastMessageTime}`}
+                            </Text>
+                        </View>
+                    );
+                }}
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
+            />
         );
     }
 }
@@ -25,6 +42,32 @@ const styles = StyleSheet.create({
         fontSize: 40,
         textAlign: 'center',
         margin: SIZES.margin
+    },
+    listItem: {
+        flex: 1,
+        flexDirection: 'row',
+        height: 70,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: COLORS.screenBackground
+    },
+    name: {
+        marginLeft: 10,
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: COLORS.primaryText
+    },
+    time: {
+        marginRight: 10,
+        fontSize: 14,
+        color: COLORS.darkText
+    },
+    flatList: {
+        backgroundColor: COLORS.screenBackground
+    },
+    separator: {
+        height: 1,
+        backgroundColor: COLORS.lightBackground
     }
 });
 
