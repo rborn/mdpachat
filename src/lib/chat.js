8e588f92296d8df4128c32fcc148ae48a44527f1
Dan Tamas <hi@dan-tamas.me>
2018-05-02 20:12:20 +0200

    add fake data

---
+++ src/lib/chat.js
@@ -0,0 +1,18 @@
+const rooms = [
+	{
+		name: 'Kickass Team 💪',
+		lastMessageTime:
+			'10:45'
+	},
+	{
+		name: 'Best Friends 🤪',
+		lastMessageTime:
+			'21:07'
+	},
+	{
+		name: 'Haloween Party 🎃',
+		lastMessageTime:
+			'00:00'
+	}
+];
+export { rooms };
---
+++ src/lib/users.js
@@ -0,0 +1,63 @@
+const users = [
+    {
+        picture: 'https://picsum.photos/100x100?image=0',
+        name: 'Celia Mendoza',
+        about:
+            'Reprehenderit nisi quis qui exercitation labore ut ea culpa cupidatat sint voluptate. Aute irure dolor ea ut est elit voluptate mollit ullamco qui aliqua aliqua. Laborum quis consequat proident nisi proident do.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=1',
+        name: 'Sharlene Sawyer',
+        about:
+            'Et irure proident magna velit sit. Aliquip qui laborum fugiat ad aliquip. Proident qui dolore irure aute dolor nisi cillum exercitation.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=2',
+        name: 'Cox Barrera',
+        about:
+            'Ad sit deserunt cillum irure commodo occaecat enim sit labore consectetur nostrud. Anim cupidatat sit ut labore do ad quis eiusmod labore. Cupidatat mollit non nulla quis voluptate incididunt sit est enim ipsum ipsum. Non aliquip anim qui ad veniam pariatur. Tempor aliqua commodo officia est elit excepteur labore non duis reprehenderit culpa dolore commodo cupidatat. Dolor qui eiusmod anim laboris.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=3',
+        name: 'Robertson Lowe',
+        about:
+            'Est eu ex elit consectetur consequat magna ad cupidatat aute. Ex commodo commodo laboris ipsum magna anim incididunt veniam velit culpa pariatur in magna voluptate. Excepteur eu aliqua do laboris ex elit. Ea aliqua eu minim ea eiusmod velit aliquip consequat voluptate pariatur. Velit do anim pariatur Lorem dolore ut consectetur ad.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=4',
+        name: 'Estelle Mcbride',
+        about:
+            'Enim veniam laborum culpa esse qui irure. Proident esse tempor laboris dolore dolore do sunt commodo fugiat. Lorem aliqua mollit laboris nulla ad aliqua voluptate nostrud ullamco magna cupidatat incididunt aliqua. Aute duis duis excepteur dolor fugiat laboris occaecat laboris. Irure cillum amet sunt eu. Laborum ipsum proident proident occaecat irure anim culpa minim irure incididunt ea. Deserunt esse proident non proident pariatur.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=5',
+        name: 'Tessa Gonzales',
+        about:
+            'Excepteur irure elit dolor eiusmod amet laborum commodo non. Nostrud pariatur duis adipisicing reprehenderit exercitation duis nostrud consectetur qui exercitation est sint. Officia non ad adipisicing eu Lorem labore dolor irure officia nostrud.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=6',
+        name: 'Olivia Middleton',
+        about:
+            'Id amet sit consequat tempor pariatur in duis. Pariatur in aliqua nisi nisi veniam labore ad tempor non sit laborum pariatur anim. Eiusmod aute proident officia magna dolor laborum nostrud nisi commodo nostrud voluptate duis aliquip.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=7',
+        name: 'Ada Vaughn',
+        about:
+            'Et amet incididunt voluptate sunt et in anim minim incididunt ex excepteur ut irure. Irure qui anim nisi exercitation sit magna occaecat irure aliqua. Ut labore laborum ipsum amet consequat sint aliqua culpa duis sint aute minim excepteur. Cupidatat enim occaecat duis pariatur dolor nisi laborum.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=8',
+        name: 'Hanson Goff',
+        about:
+            'Dolor sint veniam nisi cillum enim laboris nostrud deserunt commodo nostrud dolore reprehenderit ut nisi. Dolore sint magna Lorem aliquip aute pariatur mollit. Ut irure fugiat pariatur non sit sit duis proident anim in dolor magna officia eiusmod. Est id laboris in anim cupidatat consequat laborum esse eu ea do reprehenderit ipsum anim. Id pariatur sint sunt in. Magna occaecat nisi minim sint veniam duis tempor minim exercitation reprehenderit nisi voluptate nisi. Ut magna nulla eiusmod et.'
+    },
+    {
+        picture: 'https://picsum.photos/100x100?image=9',
+        name: 'Maude Hyde',
+        about:
+            'Magna commodo non ullamco ex veniam amet deserunt ad aliquip eu mollit commodo qui tempor. Incididunt amet anim enim irure ad cillum ut sint consequat amet. Nisi nulla elit sunt cillum nostrud aliqua. Deserunt pariatur aute sit nostrud reprehenderit exercitation aute magna deserunt dolore mollit commodo. Ut elit magna duis excepteur duis Lorem. Dolor voluptate dolor aliqua officia id velit.'
+    }
+];
+export { users };
