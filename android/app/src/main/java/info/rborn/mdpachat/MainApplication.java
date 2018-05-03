23cac83efc70d6a491dc8d21cd24945a11fd4812
Dan Tamas <hi@dan-tamas.me>
2018-05-03 12:17:09 +0200

    add firebase.database module

--- android/app/src/main/java/info/rborn/mdpachat/MainApplication.java
+++ android/app/src/main/java/info/rborn/mdpachat/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage(),
-            new RNFirebaseAuthPackage()
+            new RNFirebaseAuthPackage(),
+            new RNFirebaseDatabasePackage()
       );
     }
 
