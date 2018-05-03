92fdab9c6a44a0769792bca544be5aa6a0e3790b
Dan Tamas <hi@dan-tamas.me>
2018-05-03 10:46:25 +0200

    cleanup unused code in index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import loginScreen from './src/screens/login';
